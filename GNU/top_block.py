6e37c1ac08ead3fcb1e7dcf6225e0c05fb146fac
lotter-ska <lkock@ska.ac.za>
2016-03-18 10:25:29 +0200

    RTL-SDR scripts

---
+++ GNU/top_block.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Thu Mar 17 09:26:25 2016
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import eng_notation
+from gnuradio import fosphor
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import osmosdr
+import time
+import wx
+
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 3e6
+        self.g = g = 90
+        self.fc = fc = 1128e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        _g_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._g_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_g_sizer,
+        	value=self.g,
+        	callback=self.set_g,
+        	label="G",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._g_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_g_sizer,
+        	value=self.g,
+        	callback=self.set_g,
+        	minimum=0,
+        	maximum=100,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_g_sizer)
+        _fc_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._fc_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_fc_sizer,
+        	value=self.fc,
+        	callback=self.set_fc,
+        	label="F",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._fc_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_fc_sizer,
+        	value=self.fc,
+        	callback=self.set_fc,
+        	minimum=900e6,
+        	maximum=1200e6,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_fc_sizer)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_c(
+        	self.GetWin(),
+        	title="Scope Plot",
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_NORM,
+        	y_axis_label="Counts",
+        )
+        self.Add(self.wxgui_scopesink2_0.win)
+        self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
+        self.rtlsdr_source_0.set_sample_rate(samp_rate)
+        self.rtlsdr_source_0.set_center_freq(fc, 0)
+        self.rtlsdr_source_0.set_freq_corr(0, 0)
+        self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
+        self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
+        self.rtlsdr_source_0.set_gain_mode(False, 0)
+        self.rtlsdr_source_0.set_gain(g, 0)
+        self.rtlsdr_source_0.set_if_gain(20, 0)
+        self.rtlsdr_source_0.set_bb_gain(20, 0)
+        self.rtlsdr_source_0.set_antenna("", 0)
+        self.rtlsdr_source_0.set_bandwidth(0, 0)
+          
+        self.fosphor_wx_sink_c_0 = fosphor.wx_sink_c(
+        	self.GetWin()
+        )
+        self.fosphor_wx_sink_c_0.set_fft_window(window.WIN_BLACKMAN_hARRIS)
+        self.fosphor_wx_sink_c_0.set_frequency_range(fc, samp_rate)
+        self.Add(self.fosphor_wx_sink_c_0.win)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.rtlsdr_source_0, 0), (self.fosphor_wx_sink_c_0, 0))    
+        self.connect((self.rtlsdr_source_0, 0), (self.wxgui_scopesink2_0, 0))    
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.fosphor_wx_sink_c_0.set_frequency_range(self.fc, self.samp_rate)
+        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+
+    def get_g(self):
+        return self.g
+
+    def set_g(self, g):
+        self.g = g
+        self._g_slider.set_value(self.g)
+        self._g_text_box.set_value(self.g)
+        self.rtlsdr_source_0.set_gain(self.g, 0)
+
+    def get_fc(self):
+        return self.fc
+
+    def set_fc(self, fc):
+        self.fc = fc
+        self._fc_slider.set_value(self.fc)
+        self._fc_text_box.set_value(self.fc)
+        self.fosphor_wx_sink_c_0.set_frequency_range(self.fc, self.samp_rate)
+        self.rtlsdr_source_0.set_center_freq(self.fc, 0)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
---
+++ specmon/sdrmon2.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 04:28:19 2016
+
+@author: ubuntu
+"""
+
+import time
+
+import os
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+Fstart="350M"
+Fstop="1G"
+avg="1"
+overlap="0"
+fftbin="16"
+gain="200"
+
+cmd="rtl_power_fftw -B /home/lotter/sdr/baseline16.txt" +" -f "+Fstart+":"+Fstop+" -g "+gain+" -n "+avg+" -o "+overlap+" -b "+fftbin+" > /home/lotter/sdr/out.txt"
+
+
+
+
+plt.ion()
+my_dpi=96
+fig = plt.figure(figsize=(1900/my_dpi, 1000/my_dpi), dpi=my_dpi)
+
+
+
+
+os.system(cmd)
+out=np.loadtxt("/home/lotter/sdr/out.txt", comments="#")
+data=np.zeros([1,len(out)])
+data[0,:]=out[:,1]
+
+try:
+
+    while True:
+        ax = fig.add_subplot(211)
+        ax.clear()
+        plt.plot(out[:,0],out[:,1])
+        plt.draw()
+        os.system(cmd)
+        out=np.loadtxt("/home/lotter/sdr/out.txt", comments="#")
+        data=np.append(data,[out[:,1].T],axis=0)
+        
+        ay = fig.add_subplot(212)
+        ay.clear()
+        plt.imshow(data)
+        plt.tight_layout()
+        
+
+except KeyboardInterrupt:
+
+    print 'zapped!'
+    print "saving"
+    np.save("/home/lotter/sdr/data",[out[:,0],data])
+    plt.savefig("/home/lotter/sdr/spectrum.png")
+    
+
+
+
+#os.system("rtl_power_fftw -f 100M:200M -o 80 -g 37 -n 10 -b 1024 > /home/lotter/sdr/temp/out.txt")
+
+#
+
+#
+
+#out=np.loadtxt("/home/lotter/sdr/temp/out.txt", comments="#")
+
+#
+
+#plot(out[:,0],out[:,1])
